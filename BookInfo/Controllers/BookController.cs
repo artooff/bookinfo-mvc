bdae7cda2a7fea8edd27176704402cf8104189f2
artooff <42548274+artooff@users.noreply.github.com>
2023-05-02 04:32:47 +0300

    first commit

---
+++ BookInfo/Controllers/BookController.cs
@@ -0,0 +1,97 @@
+﻿using BookInfo.Data;
+using BookInfo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookInfo.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Book> booksList = _db.Books;
+            return View(booksList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book obj)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Books.AddAsync(obj);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var book = await _db.Books.FindAsync(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //POST 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Book obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Books.Update(obj);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var book = await _db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //POST 
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(Book obj)
+        {
+            _db.Books.Remove(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ BookInfo/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookInfo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookInfo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
